77d5d4a449e098d7f6a628584cb6f2ee179dec6a
Angju <107686204+digital-deceit@users.noreply.github.com>
2025-04-11 15:41:38 -0400

    Feat: Added Nerdfont and Ripgrep blink.cmp sources dependencies.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -47,8 +47,11 @@ now (function ()
     source = 'saghen/blink.cmp',
     depends = {
       'rafamadriz/friendly-snippets',
+      'MahanRahmati/blink-nerdfont.nvim',
       'moyiz/blink-emoji.nvim',
+      'mikavilpas/blink-ripgrep.nvim',
       'Kaiser-Yang/blink-cmp-git',
+      'brenoprata10/nvim-highlight-colors',
     },
     checkout = 'v1.1.0',
   })
@@ -117,27 +120,62 @@ now (function ()
         window = { border = 'solid' },
       },
     },
+
+    keymap = { preset = 'default' },
+
+    appearance = {
+      nerd_font_variant = 'mono',
+    },
+
     sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer', 'git', 'emoji', 'fish', 'nerdfont', 'lazydev' },
+      default = { 'lsp', 'path', 'snippets', 'buffer', 'nerdfont', 'emoji', 'ripgrep', 'git', 'lazydev' },
       providers = {
-        lazydev = {
-          name = 'LazyDev',
-          module = 'lazydev.integrations.blink',
-          score_offset = 100,
-        },
-        git = {
-          module = 'blink-cmp-git',
-          name = 'Git',
+        nerdfont = {
+          module = 'blink-nerdfont',
+          name = 'Nerd Fonts',
+          score_offset = 15,
+          opts = { insert = true },
         },
         emoji = {
           module = 'blink-emoji',
           name = 'Emoji',
           score_offset = 15,
+          opts = { insert = true },
         },
+        ripgrep = {
+          module = 'blink-ripgrep',
+          name = 'Ripgrep',
+          ---@module "blink-ripgrep"
+          ---@type blink-ripgrep.Options
+          opts = {},
+          transform_items = function (_, items)
+            for _, item in ipairs (items) do
+              item.labelDetails = {
+                description = '(rg)',
+              }
+            end
+            return items
+          end,
         },
+        git = {
+          module = 'blink-cmp-git',
+          name = 'Git',
+          enabled = function ()
+            return vim.tbl_contains ({ 'octo', 'gitcommit', 'markdown' }, vim.bo.filetype)
+          end,
+          opts = {
+            -- options for the blink-cmp-git
+          },
+        },
+        lazydev = {
+          name = 'LazyDev',
+          module = 'lazydev.integrations.blink',
+          score_offset = 100,
         },
       },
     },
+
+    fuzzy = { implementation = 'prefer_rust_with_warning' },
   })
 end)
 

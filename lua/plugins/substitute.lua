5d361a22c4d194ca0cbcaf075814e700e3d16cda
Angju <107686204+digital-deceit@users.noreply.github.com>
2025-04-26 17:02:00 -0400

    Feat: Added exchange operator keymaps [Substitue].

--- lua/plugins/substitute.lua
+++ lua/plugins/substitute.lua
@@ -8,6 +8,8 @@ later (function ()
   })
 
   local subs = require ('substitute')
+  local subs_range = require ('substitute.range')
+  local subs_exchange = require ('substitute.exchange')
 
   subs.setup ({
     on_substitute = function ()
@@ -15,8 +17,15 @@ later (function ()
     end,
   })
 
-  map ('<leader>s', subs.operator, 'Substitute operator')
-  map ('<leader>ss', subs.line, 'Substitute line')
-  map ('<leader>S', subs.eol, 'Substitute eol')
-  map ('<leader>s', subs.visual, 'Substitute visual', 'x')
+  map ('<leader>so', subs.operator, 'Operator [Substitute')
+  map ('<leader>sl', subs.line, 'Line [Substitute')
+  map ('<leader>se', subs.eol, 'Eol [Substitute]')
+  map ('<leader>sx', subs.visual, 'Visual [Substitute]', 'x')
+  map ('<leader>sr', subs_range.operator, 'Operator Range [Substitute]')
+  map ('<leader>sX', subs_range.visual, 'Visual Range [Substitute]', 'x')
+  map ('<leader>sw', subs_range.word, 'Word Range [Substitute]')
+  map ('<leader>sR', subs_exchange.operator, 'Exchange Operator [Substitute]')
+  map ('<leader>sL', subs_exchange.line, 'Exchange Line [Substitute]')
+  map ('<leader>sv', subs_exchange.visual, 'Exchange Visual [Substitute]', 'x')
+  map ('<leader>sq', subs_exchange.cancel, 'Exchange Cancel [Substitute]')
 end)

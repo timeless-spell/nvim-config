6abc23805d61ba6add29b572e8eb413480b618f2
Angju <107686204+digital-deceit@users.noreply.github.com>
2025-05-05 19:00:48 -0400

    Added icons and modified keymaps.

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -18,49 +18,43 @@ later (function ()
   dapui.setup ()
   dap_virtual_text.setup ()
 
-  dap.listeners.after.event_initialized['dapui_config'] = dapui.open
-  dap.listeners.before.event_terminated['dapui_config'] = dapui.close
-  dap.listeners.before.event_exited['dapui_config'] = dapui.close
+  -- Change breakpoint icons
+  vim.api.nvim_set_hl (0, 'DapBreak', { fg = '#e51400' })
+  vim.api.nvim_set_hl (0, 'DapStop', { fg = '#ffcc00' })
+  local breakpoint_icons = vim.g.have_nerd_font
+      and {
+        Breakpoint = '',
+        BreakpointCondition = '',
+        BreakpointRejected = '',
+        LogPoint = '',
+        Stopped = '',
+      }
+    or {
+      Breakpoint = '●',
+      BreakpointCondition = '⊜',
+      BreakpointRejected = '⊘',
+      LogPoint = '◆',
+      Stopped = '⭔',
+    }
+  for type, icon in pairs (breakpoint_icons) do
+    local tp = 'Dap' .. type
+    local hl = (type == 'Stopped') and 'DapStop' or 'DapBreak'
+    vim.fn.sign_define (tp, { text = icon, texthl = hl, numhl = hl })
+  end
 
-  map ('<leader>db', dap.toggle_breakpoint, 'Toggle Breakpoint [DAP]')
-  map ('<leader>dB', dap.toggle_breakpoint, 'Set Breakpoint [DAP]')
-  map ('<leader>dc', dap.continue, 'Start/Continue [DAP]')
-  map ('<leader>dx', dap.restart, 'Restart [DAP]')
-  map ('<leader>di', dap.step_into, 'Step Into [DAP]')
-  map ('<leader>dv', dap.step_over, 'Step Over [DAP]')
-  map ('<leader>do', dap.step_out, 'Step Out [DAP]')
-  map ('<leader>dk', dap.step_back, 'Step Back [DAP]')
-  map ('<leader>dq', dap.terminate, 'Terminate [DAP]')
-  map ('<leader>dh', function ()
-    require ('dap.ui.widgets').hover ()
-  end, 'Hover [DAP]', { 'n', 'v' })
-  map ('<leader>dp', function ()
-    require ('dap.ui.widgets').preview ()
-  end, 'Preview [DAP]', { 'n', 'v' })
-  map ('<leader>df', function ()
-    local widgets = require ('dap.ui.widgets')
-    widgets.centered_float (widgets.frames)
-  end, 'Float frame [DAP]')
-  map ('<leader>ds', function ()
-    local widgets = require ('dap.ui.widgets')
-    widgets.centered_float (widgets.scopes)
-  end, 'Float scopes [DAP]')
-
-  vim.api.nvim_create_user_command ('DAPUIopen', function ()
+  dap.listeners.before.attach.dapui_config = function ()
     dapui.open ()
-  end, {})
-
-  vim.api.nvim_create_user_command ('DAPUIclose', function ()
+  end
+  dap.listeners.before.launch.dapui_config = function ()
+    dapui.open ()
+  end
+  dap.listeners.before.event_terminated.dapui_config = function ()
     dapui.close ()
-  end, {})
+  end
+  dap.listeners.before.event_exited.dapui_config = function ()
+    dapui.close ()
+  end
 
-  vim.api.nvim_create_user_command ('DAPUItoggle', function ()
-    dapui.toggle ()
-  end, {})
-
-  vim.api.nvim_create_user_command ('DAPUIfloat', function ()
-    dapui.float_element ()
-  end, {})
   dap.adapters.local_lua = {
     type = 'executable',
     command = 'node',
@@ -78,9 +72,6 @@ later (function ()
     end,
   }
 
-  vim.api.nvim_create_user_command ('DAPUIeval', function ()
-    dapui.eval (nil, { enter = true })
-  end, {})
   dap.configurations.lua = {
     {
       name = 'Current file (local-lua-dbg, lua)',
@@ -136,4 +127,30 @@ later (function ()
       vmArgs = '' .. '-Xmx2g ',
     },
   }
+
+  map ('<leader>Db', dap.toggle_breakpoint, 'Toggle Breakpoint [DAP]')
+  map ('<leader>DB', function ()
+    dap.set_breakpoint (vim.fn.input ('Breakpoint condition: '))
+  end, 'Set Breakpoint [DAP]')
+  map ('<F5>', dap.continue, 'Start/Continue [DAP]')
+  map ('<F1>', dap.step_into, 'Step Into [DAP]')
+  map ('<F2>', dap.step_over, 'Step Over [DAP]')
+  map ('<F3>', dap.step_out, 'Step Out [DAP]')
+  map ('<F4>', dap.step_back, 'Step Back [DAP]')
+  map ('<F8>', dap.restart, 'Restart [DAP]')
+  map ('<F9>', dap.terminate, 'Terminate [DAP]')
+  map ('<leader>Dh', function ()
+    require ('dap.ui.widgets').hover ()
+  end, 'Hover [DAP]', { 'n', 'v' })
+  map ('<leader>Dp', function ()
+    require ('dap.ui.widgets').preview ()
+  end, 'Preview [DAP]', { 'n', 'v' })
+  map ('<leader>Df', function ()
+    local widgets = require ('dap.ui.widgets')
+    widgets.centered_float (widgets.frames)
+  end, 'Float frame [DAP]')
+  map ('<leader>Ds', function ()
+    local widgets = require ('dap.ui.widgets')
+    widgets.centered_float (widgets.scopes)
+  end, 'Float scopes [DAP]')
 end)
